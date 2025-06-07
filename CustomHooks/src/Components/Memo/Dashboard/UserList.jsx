f3e32790e336a6b582db5e39a3de2ce3af57f18d
Sandhya <sandhyarana07610761@gmail.com>
2025-06-07 17:06:18 +0530

    commit

--- CustomHooks/src/Components/Memo/Dashboard/UserList.jsx
+++ CustomHooks/src/Components/Memo/Dashboard/UserList.jsx
@@ -15,3 +15,7 @@ const UsersList = ({ users }) => {
 };
 export default UsersList
 /**useMemo(() => computeSomething, [dependencies]) is a React Hook that memoizes a value so that the expensive calculation doesn’t run again unless needed. */
+/* react.memo: when a component is rerendered the children also get rerendered so we cover the children component in memo so that it doesn't get rerendered.
+react.callback-function...when we use react.memo the immutable strings as prop do not rerender the component but the same prop that is a function will always rerender the component so we use react.callback...
+useMemo-when we do a calculation over some things...if the things do not change the calculation will not change that is why we add the
+"things" in the dependency in useMemo */
